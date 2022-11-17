adb4f78fec1e6cee004a53495544a3566a56e1c9
iTemat <paweladamian@gmail.com>
2022-11-17 10:22:56 +0100

    Test commit

--- playground-template/src/App.jsx
+++ playground-template/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <main>
       <div className="max-w-screen-lg mx-auto py-20 px-5">
-        <h1 className="text-4xl">React Playground</h1>
+        <h1 className="text-4xl">React Playground 1</h1>
         <Module1 />
         <Module2>
           <p>I can take children!</p>
